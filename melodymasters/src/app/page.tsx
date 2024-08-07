53a5546f877a9afbb14a4f85f52382bebf429a0d
sumit <prajapatisumit009@gmail.com>
2024-08-07 17:13:53 +0530

    Creating a navbar and giving a black theme

--- melodymasters/src/app/page.tsx
+++ melodymasters/src/app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-   <h1 className="text-2xl flex items-center justify-center h-screen">
-    Start Next js 
-   </h1>
+   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white">
+      <h1 className="text-2xl flex items-center justify-center h-screen">
+        Start Next js 
+      </h1>
+   </main>
   );
 }
