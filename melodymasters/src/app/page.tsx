221742ef7cf25c20adee32630a1c102479ec139b
sumit <prajapatisumit009@gmail.com>
2024-08-07 19:10:09 +0530

    Adding Movingg Border effect .

--- melodymasters/src/app/page.tsx
+++ melodymasters/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">s
       <HeroSection/>
+      
    </main>
   );
 }
--- melodymasters/src/components/HeroSection.tsx
+++ melodymasters/src/components/HeroSection.tsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import Link from "next/link";
 import { Spotlight } from './ui/Spotlight';
+import { Button } from "./ui/moving-border";
 
 export default class HeroSection extends Component {
   render() {
@@ -11,18 +12,21 @@ export default class HeroSection extends Component {
         <div className='p-4 relative z-10 w-full text-center' >
             
         <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
+        className="-top-40 left-0 md:left-60 md:-top-10"
         fill="white"/>
-        
-            <h1 className='mt-20 md:mt-0 text-4xl md:text-6xl font-bold bg-clip-text 
+
+            <h1 className='mt-20 md:mt-0 text-4xl md:text-5xl font-bold bg-clip-text 
             text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'> Master the art of music </h1>
             
-            <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'> Dive into our comprehensive music courses and transform your musical journey today. Whether you're a
+            <p className='mt-4 font-normal text-base md:text-sm text-neutral-300 max-w-lg mx-auto'> Dive into our comprehensive music courses and transform your musical journey today. Whether you're a
                  beginner or looking to refine your skills, join us to unlock your true potential.</p>
-            <div className="mt-4">
-                <Link href={"/courses"}>
-                Explore Courses
-                </Link>
+            <div className="mt-3">
+           
+            <Button
+                borderRadius="1.75rem"
+                className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >Explore Courses </Button>
+            
             </div>
         </div>
       </div>
