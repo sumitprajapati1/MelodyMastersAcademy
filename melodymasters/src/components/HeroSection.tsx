116d6a52915dab9b03e47182d0f817de0f752a0c
sumit <prajapatisumit009@gmail.com>
2024-08-07 18:30:03 +0530

    Adding Spotlight effect .

--- melodymasters/src/components/HeroSection.tsx
+++ melodymasters/src/components/HeroSection.tsx
@@ -1,15 +1,19 @@
 "user client";
 import React, { Component } from 'react';
 import Link from "next/link";
+import { Spotlight } from './ui/Spotlight';
 
 export default class HeroSection extends Component {
   render() {
     return (
       <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col justify-center item-center mx-auto overflow-hidden
       py-10 md:py-0 text-white'>
-        
         <div className='p-4 relative z-10 w-full text-center' >
             
+        <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"/>
+        
             <h1 className='mt-20 md:mt-0 text-4xl md:text-6xl font-bold bg-clip-text 
             text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'> Master the art of music </h1>
             
--- melodymasters/tailwind.config.ts
+++ melodymasters/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const colors=require("tailwindcss/colors");
 
 const config: Config = {
   content: [
@@ -9,6 +10,21 @@ const config: Config = {
   darkMode:"class",
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+       keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: '1',
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
