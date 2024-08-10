14c2e428235c134af880f1563b323f2cde58252f
sumit <prajapatisumit009@gmail.com>
2024-08-10 22:39:53 +0530

    Creating Grid blocks

--- melodymasters/src/components/TestimonialCads.tsx
+++ melodymasters/src/components/TestimonialCads.tsx
@@ -5,8 +5,9 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 export class TestimonialCads extends Component {
     render() {
         return (
-            <div  className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
-                <h2 className="text-3xl text-white font-bold text-center mb-8 z-10">Hear Our Harmony : Voices of Success </h2>
+            <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+                <h2 className="text-2xl sm:text-4xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b
+                 from-neutral-200 to-neutral-500 py-8"> Harmony : Voices of Success </h2>
                 <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
                     <div  className="w-full max-w-6xl">
                         <InfiniteMovingCards
