52ce8515dc6fdc0350df9f75bc1915b189d197da
sumit <prajapatisumit009@gmail.com>
2024-08-11 21:06:12 +0530

    Creating Upcoming Webinars Page and Wavy Background Effect

--- melodymasters/src/app/page.tsx
+++ melodymasters/src/app/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCads from "@/components/TestimonialCads";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
-   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">s
-      <HeroSection/>
-      <FeaturedCourses/>
-     <WhyChooseUs/> 
-    <TestimonialCads/>
-   </main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCads />
+      <UpcomingWebinars />
+      <Instructors />
+    </main>
   );
 }
